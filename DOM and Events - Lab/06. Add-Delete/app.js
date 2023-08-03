64b216d2c47859698da76b810e831c2e2d4cf48b
GabrielYH <gabrielyhristov@gmail.com>
2023-08-03 08:58:03 +0300

    Adding Exercises

---
+++ DOM and Events - Lab/06. Add-Delete/app.js
@@ -0,0 +1,16 @@
+function addItem() {
+  const value = document.querySelector("#newItemText").value;
+
+  const item = document.createElement("li");
+  item.textContent = value;
+
+  const deleteButton = document.createElement("a");
+  deleteButton.href = "#";
+  deleteButton.textContent = "[Delete]";
+  item.appendChild(deleteButton);
+  deleteButton.addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+  });
+
+  document.querySelector("ul").appendChild(item);
+}
